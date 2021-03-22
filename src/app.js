db52b4090311e1f4221e18557ad08637d67f6066
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2021-03-22 11:56:22 -0300

    express

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ "extended": "true" }));
 app.use(bodyParser.json());                                     
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routers);
 
 
