034353090540f83300bb790b9be7f1472e0be93a
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2021-03-22 07:50:16 -0300

    server

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {
 	port: process.env.DB_URL || 3000,
-	db: process.env.MONGOLAB_URI || "mongodb://localhost/pokedex",
+	db: process.env.MONGODB_URI  || "mongodb://localhost/pokedex",
 	test_port: 2001,
 	test_db: "mongodb://localhost/pokedex"
 };
--- src/server.js
+++ src/server.js
@@ -4,22 +4,16 @@ const config = require("../config/config");
 const mongoose = require("mongoose");
 const PokemonController = require("../api/controllers/pokemon.controller")
 var server;
-if (process.env.NODE_ENV === "test") {
-	mongoose.connect(config.test_db);
-	server = app.listen(config.test_port, function (err) {
-		if (err){
-			throw err;
-		}
-	});
-} else {
+const port = process.env.PORT || 3000;
+
 	mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true });
-	server = app.listen(config.port, function (err) {
+	server = app.listen(port, function (err) {
         console.log(`App listening on port ${config.port}`)
 		PokemonController.findAllAndCreateExternalAPI();
 		if (err){
 			throw err;
 		}
 	});
-}
+
 
 module.exports = server;
