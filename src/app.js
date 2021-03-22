5c21b744048a405d3a8e145c20ca672bbd744a4b
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2021-03-22 12:50:07 -0300

    route

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const routers = require("../routers/index.router");
-
+const path = require('path');
                                       
 app.use(cors());
 app.use(bodyParser.urlencoded({ "extended": "true" }));            
 app.use(bodyParser.json());                                     
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
 
-app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, '../routers')));
+console.log()
 app.use(routers);
 
 
